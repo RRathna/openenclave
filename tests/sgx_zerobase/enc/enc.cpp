51166be412a8eaa619af82d4f7a3fe975a9bab4f
Rathna Ramesh <rathna1993@gmail.com>
2021-07-13 16:32:15 +0000

    initial commit
    Signed-off-by: Rathna Ramesh <rathna1993@gmail.com>

--- tests/sgx_zerobase/enc/enc.cpp
+++ tests/sgx_zerobase/enc/enc.cpp
@@ -3,42 +3,41 @@
 
 #include <openenclave/enclave.h>
 #include <openenclave/internal/tests.h>
+#include <sys/mman.h>
+#include <iostream>
 #include "sgx_zerobase_t.h"
 
-const char* protected_message = "Hello world from Enclave\n\0";
-
-int secure_string_patching(
-    const char* source,
-    char* output,
-    size_t output_length)
+int test_enclave_mmap(uint64_t addr)
 {
-    const char* running_source = source;
-    size_t running_length = output_length;
-    while (running_length > 0 && *running_source != '\0')
-    {
-        *output = *running_source;
-        running_length--;
-        running_source++;
-        output++;
-    }
-    const char* ptr = protected_message;
-    while (running_length > 0 && *ptr != '\0')
-    {
-        *output = *ptr;
-        running_length--;
-        ptr++;
-        output++;
-    }
-    if (running_length < 1)
-    {
+    void* loc = mmap(
+        (void*)addr,
+        1,
+        PROT_READ | PROT_WRITE,
+        MAP_SHARED | MAP_ANONYMOUS,
+        -1,
+        0);
+
+    if (loc != (void*)addr)
+        return 1;
+
+    munmap(loc, 1);
+    return 0;
+}
+
+int test_ecall(const char* message)
+{
+    if (!message)
         return -1;
-    }
-    *output = '\0';
-    int rval = -1;
-    OE_TEST(
-        unsecure_string_patching(&rval, source, output, output_length) ==
-        OE_OK);
-    return rval;
+    else
+        fprintf(stdout, "[enclave] Message from host : %s\n", message);
+
+    int res = -1;
+    const char* input = "testing ocall\0";
+    OE_TEST(test_ocall(&res, input) == OE_OK);
+    if (res != 0)
+        fprintf(stderr, "[enclave] ocall failed %d\n", res);
+
+    return res;
 }
 
 OE_SET_ENCLAVE_SGX2(
--- tests/sgx_zerobase/host/host.cpp
+++ tests/sgx_zerobase/host/host.cpp
@@ -3,38 +3,37 @@
 
 #include <openenclave/host.h>
 #include <openenclave/internal/tests.h>
+#include <sys/mman.h>
 #include <iostream>
 #include <vector>
 #include "sgx_zerobase_u.h"
 
 const char* message = "Hello world from Host\n\0";
 
-int unsecure_string_patching(
-    const char* source,
-    char* output,
-    size_t output_length)
+int test_ocall(const char* message)
 {
-    size_t running_length = output_length;
-    while (running_length > 0 && *source != '\0')
-    {
-        *output = *source;
-        running_length--;
-        source++;
-        output++;
-    }
-    const char* ptr = message;
-    while (running_length > 0 && *ptr != '\0')
-    {
-        *output = *ptr;
-        running_length--;
-        ptr++;
-        output++;
-    }
-    if (running_length < 1)
-    {
+    if (!message)
         return -1;
-    }
-    *output = '\0';
+
+    fprintf(stdout, "[host] Message from enclave : %s\n", message);
+
+    return 0;
+}
+
+int test_host_mmap(uint64_t addr)
+{
+    void* loc = mmap(
+        (void*)addr,
+        1,
+        PROT_READ | PROT_WRITE,
+        MAP_SHARED | MAP_ANONYMOUS,
+        -1,
+        0);
+
+    if (loc != (void*)addr)
+        return 1;
+
+    munmap(loc, 1);
     return 0;
 }
 
@@ -45,44 +44,105 @@ int main(int argc, const char* argv[])
 
     if (argc != 2)
     {
-        fprintf(
-            stderr,
-            "Usage: sgx_zerobase_host.exe <path to  packaged enc/dev dll>\n"
-            "Example: sgx_zerobase_host.exe "
-            "sgx_zerobase_enc.dev.pkg\\sgx_zerobase_enc.dll\n");
+        fprintf(stderr, "Usage: %s ENCLAVE_PATH\n", argv[0]);
         return 1;
     }
 
+    fprintf(
+        stdout,
+        "\nTest 1: Create 0-base enclave in sim-mode\n"
+        "Expected result : OE_INVALID_PARAMETER\n");
+    result = oe_create_sgx_zerobase_enclave(
+        argv[1],
+        OE_ENCLAVE_TYPE_SGX,
+        OE_ENCLAVE_FLAG_SIMULATE,
+        NULL,
+        0,
+        &enclave);
+
+    if (result != OE_INVALID_PARAMETER)
+    {
+        fprintf(
+            stderr,
+            "Unexpected error when creating enclave in sim-mode, result=%d\n",
+            result);
+        return 1;
+    }
+
+    fprintf(
+        stdout,
+        "\nTest 2: Create 0-base enclave\n"
+        "Expected result : OE_OK\n");
     const uint32_t flags = oe_get_create_flags();
 
     result = oe_create_sgx_zerobase_enclave(
         argv[1], OE_ENCLAVE_TYPE_SGX, flags, NULL, 0, &enclave);
     if (result != OE_OK)
     {
-        fprintf(stderr, "Could not create enclave, result=%d\n", result);
+        fprintf(stderr, "Could not create 0-base enclave, result=%d\n", result);
         return 1;
     }
-    char output[1024];
-    const char* source = "My First App\n";
+
+    fprintf(
+        stdout,
+        "\nTest 3: Test ecall, ocall on 0-base enclave\n"
+        "Expected result : OE_OK\n");
+    const char* input = "testing ecall\0";
     int res = -1;
-    OE_TEST(
-        secure_string_patching(
-            enclave, &res, source, output, OE_COUNTOF(output)) == OE_OK);
+    OE_TEST(test_ecall(enclave, &res, input) == OE_OK);
 
     if (res != 0)
     {
-        fprintf(stderr, "%s: enclave called failed\n", argv[0]);
-        exit(1);
+        fprintf(stderr, "[host]: ecall/ocall failed %d\n", res);
+        return 1;
     }
 
-    const char expect[] = "My First App\n"
-                          "Hello world from Enclave\n"
-                          "My First App\n"
-                          "Hello world from Host\n";
+    fprintf(
+        stdout,
+        "\nTest 4: Check host access to [/proc/sys/vm/mmap_min_addr, "
+        "elrange->image_start_address]\n"
+        "Expected result : successful\n");
 
-    if (strcmp(output, expect) != 0)
+    uint64_t addr = 0x15000;
+
+    if (test_host_mmap(addr) != 0)
     {
-        fprintf(stderr, "%s: returned string don't match\n", argv[0]);
+        fprintf(stderr, "host mmap at address %lx failed\n", addr);
+        return 1;
+    }
+
+    fprintf(
+        stdout,
+        "\nTest 5: Check enclave access to [enclave_image_start_address, "
+        "enclave_image_end_address]\n"
+        "Expected result : successful\n");
+
+    addr = 0x35000;
+    OE_TEST(test_enclave_mmap(enclave, &res, addr) == OE_OK);
+    if (res != 0)
+    {
+        fprintf(stderr, "enclave mmap at address %lx failed\n", addr);
+        return 1;
+    }
+
+    fprintf(
+        stdout,
+        "\nTest 6: Check host access to 0x0\n"
+        "Expected result : failed\n");
+
+    addr = 0x0;
+
+    if (!test_host_mmap(addr))
+    {
+        fprintf(stderr, "host mmap at address %lx failed\n", addr);
+    }
+    else
+        return 1;
+
+    OE_TEST(test_enclave_mmap(enclave, &res, addr) == OE_OK);
+    if (res != 0)
+    {
+        fprintf(stderr, "enclave mmap at address %lx failed\n", addr);
         return 1;
     }
 
